201983eccd88eab71ca0fc1922ff1982f6d6c60c
runner <1493743519@qq.com>
2019-08-19 09:36:09 +0800

    深克隆、浅克隆的使用

--- src/main/java/com/company/springdemo/test/bean/clone/Person.java
+++ src/main/java/com/company/springdemo/test/bean/clone/Person.java
@@ -1,7 +1,6 @@
 package com.company.springdemo.test.bean.clone;
 
 import lombok.Data;
-import org.junit.jupiter.api.Test;
 
 import java.io.*;
 
@@ -43,38 +42,6 @@ public class Person implements Cloneable {
         Address a = ((Person) obj).getAddress();
         ((Person) obj).setAddress((Address) a.clone());
         return obj;
-    }
-
-    @Test
-    public void testShallowCopy() throws Exception {
-        Person p1 = new Person();
-        p1.setAge(31);
-        p1.setName("Peter");
-
-        Person p2 = (Person) p1.clone();
-        System.out.println(p1 == p2);//false
-        p2.setName("Jacky");
-        System.out.println("p1=" + p1);//p1=Person [name=Peter, age=31]
-        System.out.println("p2=" + p2);//p2=Person [name=Jacky, age=31]
-    }
-
-    @Test
-    public void testShallowCopy1() throws Exception {
-        Address address = new Address();
-        address.setType("Home");
-        address.setValue("北京");
-
-        Person p1 = new Person();
-        p1.setAge(31);
-        p1.setName("Peter");
-        p1.setAddress(address);
-
-        Person p2 = (Person) p1.clone();
-        System.out.println(p1 == p2);//false
-
-        p2.getAddress().setType("Office");
-        System.out.println("p1=" + p1);
-        System.out.println("p2=" + p2);
     }
 
 }
