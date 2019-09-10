4418aa1321a357e2672f182df79dfd6cec4a7ca8
runner <1493743519@qq.com>
2019-09-10 14:19:16 +0800

    装饰者模式、简单工厂模式、工厂方法模式、抽象工厂模式练习

---
+++ src/main/java/com/company/springdemo/test/design/decorator/PersonBefore.java
@@ -0,0 +1,12 @@
+package com.company.springdemo.test.design.decorator;
+
+/**
+ * @description: 该类 被装饰的类 (被增强对象)
+ * @author: whs
+ * @date: 2019/09/09 18:05
+ */
+public class PersonBefore {
+    public void eat() {
+        System.out.println("吃饭");
+    }
+}
